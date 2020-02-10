1923eca6485e9e43a1a95cace89b1ad8dbf6ca84
maladr0it <bradford.lachlan@gmail.com>
2020-02-10 13:27:18 +0900

    remove dnd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { Board } from "components/Board";
-import { Hand } from "components/Hand";
-
-export const App: React.FC = () => {
+const App = () => {
   return (
-    <>
-      <Board />
-      <Hand />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
+
+export default App;
--- client/src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const clamp = (lower: number, upper: number, value: number) => {
-  return Math.min(Math.max(value, lower), upper);
-};
